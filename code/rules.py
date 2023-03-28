24475a962bdd3e2ddf2c111c7f925f96b06ca592
vitalex93 <vitalex93@gmail.com>
2023-03-28 11:47:43 +0300

    rules development

--- code/rules.py
+++ code/rules.py
@@ -1,25 +1,27 @@
 import spacy
 from spacy import displacy
 from spacy.pipeline import EntityRuler
+from helper import *
 
-nlp = spacy.blank("en")
-#ruler = EntityRuler(nlp)
-ruler = nlp.add_pipe('entity_ruler')
 
-patterns = [{"label": "APPTYPE", "pattern":  [{'LOWER': 'application'},{'LOWER': 'type'}]},
-            {"label": "DCA", "pattern": [{"LOWER": "per", 'OP':'?'}, {"LOWER": "dca"}]}]
+def create_ruler(patterns, model):
+    """Create a spaCy rule-based `EntityRuler` object with the provided patterns."""
+    nlp = model
+    ruler = EntityRuler(model, overwrite_ents=True)
+    ruler = nlp.add_pipe('entity_ruler')
+    ruler.add_patterns(patterns)
+    return ruler
 
-ruler.add_patterns(patterns)
-#nlp.add_pipe(ruler, before="ner")
-
-Q7 = 'Create a report that shows the number of settlement applications approved during the month, their approved amount, the written off balance,\
-      the average days to approval, their average and median duration and their entry principal and balance,\
-          for September 2020 DCA and application type. The report should be produced on Earth portfolio.'
-doc = nlp(Q7)
-for ent in doc.ents:
-    print(ent.text, ent.label_)
-
-displacy.render(doc, style='ent')
+def get_entities_by_label(text, labels, model, patterns):
+    """Get a list of named entities in the given `Doc` object that match the specified labels."""
+    ruler = create_ruler(patterns=patterns, model=model)
+    nlp = model
+    doc = nlp(text)
+    entities = []
+    for ent in doc.ents:
+        if ent.label_ in labels and ent in ruler(doc).ents:
+            entities.append(ent.text)
+    return entities
 
 
 
