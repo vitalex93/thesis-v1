557454dcce943f0f2d09ea9870b5f8cf89d0220c
vitalex93 <vitalex93@gmail.com>
2023-03-31 11:34:58 +0300

    doc classifier intial commit

---
+++ code/DocumentClassifier.py
@@ -0,0 +1,26 @@
+import spacy
+from spacy.matcher import Matcher
+from collections import Counter
+
+class DocumentClassifier:
+    def __init__(self, pattern_dict):
+        self.nlp = spacy.load("en_core_web_sm")
+        self.matchers = {}
+        for category, patterns in pattern_dict.items():
+            matcher = Matcher(self.nlp.vocab)
+            #for pattern in patterns:
+            matcher.add(category, patterns)
+            self.matchers[category] = matcher
+    
+    def classify(self, text):
+        doc = self.nlp(text)
+        category_counts = Counter()
+        for category, matcher in self.matchers.items():
+            matches = matcher(doc)
+            count = len(matches)
+            category_counts[category] += count
+        if not category_counts:
+            return None
+        most_common_category, _ = category_counts.most_common(1)[0]
+        category_ranking = category_counts.most_common()
+        return most_common_category, category_ranking
--- code/rules.py
+++ code/rules.py
@@ -4,6 +4,7 @@ from spacy.pipeline import EntityRuler
 from helper import *
 
 
+
 def create_ruler(patterns, model):
     """Create a spaCy rule-based `EntityRuler` object with the provided patterns."""
     nlp = model
@@ -18,10 +19,14 @@ def get_entities_by_label(text, labels, model, patterns):
     ruler = create_ruler(patterns=patterns, model=model)
     nlp = model
     doc = nlp(text)
-    entities = []
+    '''entities = []
     for ent in doc.ents:
         if ent.label_ in labels and ent in ruler(doc).ents:
-            entities.append(ent.text)
+            entities.append(ent.text)'''
+    entities = {}
+    for ent in doc.ents:
+        if ent.label_ in labels and ent in ruler(doc).ents:
+            entities[str(ent.label_)] = ent.text
     return entities
 
 def get_entities_by_dict(text, labels, model, patterns, label_dict):
@@ -31,6 +36,25 @@ def get_entities_by_dict(text, labels, model, patterns, label_dict):
         entities = get_entities_by_label(text, labels, model, patterns)
         entities_dict[key] = entities
     return entities_dict
+
+
+def classify_docs(docs, model, patterns):
+    nlp = model
+    ruler = create_ruler(patterns=patterns, model=nlp)
+    return [doc.ents[0].label_ if doc.ents else None for doc in nlp.pipe(docs)]
+
+'''def classify_docs(docs):
+    nlp = spacy.blank("en")
+    ruler = EntityRuler(nlp)
+    patterns = [{'label':'RowGroup4', 'pattern': [{'LOWER': 'application'},{'LOWER': 'type'}]},
+                {'label':'RowGroup4', 'pattern': [{"LOWER": "per", 'OP':'?'}, {"LOWER": "dca"}]},
+                {'label':'RowGroup1', 'pattern': [{'LEMMA': 'payment'}]},
+                {'label':'RowGroup1', 'pattern': [{'LEMMA': 'account'}]}]
+    ruler.add_patterns(patterns)
+    ruler = nlp.add_pipe('entity_ruler')
+
+    return [doc.ents[0].label_ if doc.ents else None for doc in nlp.pipe(docs)]'''
+
 
 
 
